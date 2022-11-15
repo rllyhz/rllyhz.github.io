c103a62ad24f7d99ba0ac2187546eaccdffda01a
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-11-15 14:34:45 +0700

    landing page

---
+++ app/landing.js
@@ -0,0 +1,18 @@
+const pinnedProjectIds = [0, 2, 1, 11, 10, 6, 5]
+
+fetch("../data/projects.json")
+.then(res => res.json())
+.then(projects => {
+  if (projects) {
+    projects.forEach((project, index) => {
+      if (pinnedProjectIds.includes(index)) {
+        addProject(project)
+      }
+    });
+  }
+
+  console.clear()
+})
+
+// STILL BEING UNDER MAINTANANCE ALERT
+alert("Sorry for any bad experiences.. 🙇‍♂️ \nThis page is still being under construction mode! 👨‍💻")
