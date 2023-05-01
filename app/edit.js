2d775dd3cbb5d00f487c66a716594ef43ea64ac7
Rully Ihza Mahendra <rullyihza00@gmail.com>
2023-05-02 05:05:07 +0700

    Updated edit page

--- app/edit.js
+++ app/edit.js
@@ -1,3 +1,10 @@
+const dataId = getQueryData("id")
+
+if (!dataId) {
+  alert("❌⛔ Activity not allowed!")
+  window.history.back();
+}
+
 const authData = getAuthData();
 
 if (!authData || !authData.isLoggedIn) {
