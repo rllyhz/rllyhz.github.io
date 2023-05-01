313ee4c5b0ebf2a811d132fd64e2af033452c4f2
Rully Ihza Mahendra <rullyihza00@gmail.com>
2023-05-02 04:47:56 +0700

    Updated detail page

--- app/detail.js
+++ app/detail.js
@@ -14,8 +14,7 @@ const imageElem = getElem(".work__img img")
 const dataId = getQueryData("id")
 
 if (!dataId) {
-  alert("❌⛔ You're not allowed!")
-  // window.location.assign("../projects/index.html")
+  alert("❌⛔ Activity not allowed!")
   window.history.back();
 }
 
@@ -26,22 +25,58 @@ if (authData && authData.isLoggedIn) {
   getElem('.nav__menu .nav__list .nav__item a').href = '/projects/edit.html?id=' + dataId;
 }
 
-fetch("../data/projects.json")
-.then(res => res.json())
-.then(projects => {
-  projects.forEach(project => {
-    if (project.id == dataId) {
-      initUI(project)
-      
-      setTimeout(() => {
-        hideElem(loadingContainer)
-        showElem(contentContainer, "flex")
-        console.clear()
-      }, 800)
-      return
-    }
-  });
+const getProjectDetailEndpointUrl = getEndpointPath(`/projects/${dataId}`);
+
+fetch(getProjectDetailEndpointUrl)
+.catch(err => {
+  console.clear();
+  initErrorUI(true);
+  return;
 })
+.then(async res => {
+  console.clear();
+  if (!res) return;
+  if (res.status == 404) {
+    alert("❌⛔ Project not found!")
+    window.history.back();
+    return null;
+  } else {
+    return await res.json();
+  }
+})
+.then(res => {
+  console.clear();
+  if (!res) return;
+  if (res.error) {
+    initErrorUI(false);
+  } else {
+    const project = res.data.project;
+    initUI(project);
+    setTimeout(() => {
+      hideElem(loadingContainer);
+      showElem(contentContainer, "flex");
+      console.clear();
+    }, 800);
+    return;
+  }
+  return;
+})
+
+function initErrorUI(isConnnectionFailed = false) {
+  hideElem(loadingContainer);
+  const errorContainerElem = document.createElement('div');
+  errorContainerElem.classList.add('error-container');
+  const errorMessageElem = document.createElement('p');
+  errorMessageElem.classList.add('error-message');
+  if (isConnnectionFailed) {
+    errorMessageElem.innerText = 'Sorry, it went wrong :( \nPlease check your connection first!';
+  } else {
+    errorMessageElem.innerText = 'Sorry :( \nSomething went wrong internally!';
+  }
+  errorContainerElem.appendChild(errorMessageElem);
+  
+  getElem('.work__container').appendChild(errorContainerElem);
+}
 
 async function initUI(project) {
   idElem.textContent = project.id
