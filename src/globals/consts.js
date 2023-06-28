619be4dc0435b6215947a964dae167e82df62666
Rully Ihza Mahendra <rullyihza00@gmail.com>
2023-06-29 06:41:36 +0700

    Updated global variables

--- src/globals/consts.js
+++ src/globals/consts.js
@@ -1,6 +1,10 @@
 const API = {};
 
 const Social = {
+  email: "rullyihza00@gmail.com",
+  linkedIn: "https://www.linkedin.com/in/rully-ihza-mahendra/",
+  github: "https://github.com/rllyhz/",
+  stackOverflow: "https://stackoverflow.com/users/17045891/rllyhz",
   facebook: "https://www.facebook.com/rully.ihza",
   instagram: "https://www.instagram.com/rllyhz/",
   twitter: "https://twitter.com/rllyhz",
