7ac6524fec5433710e2d5603f24722e37d286f60
Rully Ihza Mahendra <rullyihza00@gmail.com>
2023-07-06 12:12:42 +0700

    Updated minor changes to the ProjectsPage

--- src/app/views/pages/ProjectsPage.js
+++ src/app/views/pages/ProjectsPage.js
@@ -59,7 +59,9 @@ export default class ProjectsPage {
       Dom.createElement({
         tagName: "p",
         innerHTML: `
-          <box-icon name='outline' color="var(--message-color)" size="6rem"></box-icon>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="width:6rem;height:6rem;fill: var(--message-color);">
+            <path d="M21 19v-9c0-1.103-.897-2-2-2h-3V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2v9c0 1.103.897 2 2 2h3v3c0 1.103.897 2 2 2h9c1.103 0 2-.897 2-2zM5 14V5h9v3h-4c-1.103 0-2 .897-2 2v4H5zm9.001 0H10v-4h4.001v4zM10 16h4c1.103 0 2-.897 2-2v-4h3l.001 9H10v-3z"></path>
+          </svg>
           </br></br>
           ${Strings.Alerts.CurrentlyNoProjects.Message} 
         `,
